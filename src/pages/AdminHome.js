00cb78db2dd731165678be3322630e8882f5254a
Abdul Rahman <110687551+87abdul@users.noreply.github.com>
2023-07-24 18:45:57 +0530

    initial commit

---
+++ src/pages/AdminHome.js
@@ -0,0 +1,16 @@
+import AdminProductList from "../features/admin/components/AdminProductList";
+import NavBar from "../features/navbar/Navbar";
+
+function AdminHome() {
+    return ( 
+        <div>
+            <NavBar>
+                <AdminProductList></AdminProductList>
+            </NavBar>
+            
+        </div>
+     );
+}
+
+export default AdminHome;
+
